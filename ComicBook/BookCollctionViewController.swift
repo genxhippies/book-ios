458c2b802e5a13265ad4fe7d2e2f57824baaea30
Jeonghwan Jang <nundefined@nundefined.com>
2015-10-13 01:04:20 +0900

    added a collection view skeleton and  its controller according to https://www.hackingwithswift.com/read/10/2/designing-uicollectionview-cells

---
+++ ComicBook/BookCollctionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BookCollctionViewController.swift
+//  ComicBook
+//
+//  Created by inome on 2015. 10. 13..
+//  Copyright © 2015년 Nundefined. All rights reserved.
+//
+
+import UIKit
+
+class BookCollctionViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ComicBook/BookCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BookCollectionViewCell.swift
+//  ComicBook
+//
+//  Created by inome on 2015. 10. 13..
+//  Copyright © 2015년 Nundefined. All rights reserved.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var bookTitle: UILabel!
+}
